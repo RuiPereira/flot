66fadad9f56b80596d270eed3573c559dbc9f627
Rui Pereira <rui.pereira@gmail.com>
2011-11-14 22:25:31 +0100

    Slimit compressed concatenation of important js files.

---
+++ compress.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cat jquery.flot.js jquery.flot.crosshair.js jquery.flot.axislabels.js \
+jquery.flot.errorbars.js jquery.flot.fillbetween.js jquery.flot.navigate.js \
+jquery.flot.selection.js jquery.flot.stack.js jquery.flot.symbol.js \
+jquery.flot.text.js > DRE.js
