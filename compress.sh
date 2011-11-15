b9c96a6d4da3bdc8bd1245de679b1b635501ce98
Rui Pereira <rui.pereira@gmail.com>
2011-11-15 11:56:27 +0100

    DRE.js -> flot.DRE.js

--- compress.sh
+++ compress.sh
@@ -2,4 +2,4 @@
 cat jquery.flot.js jquery.flot.crosshair.js jquery.flot.axislabels.js \
 jquery.flot.errorbars.js jquery.flot.fillbetween.js jquery.flot.navigate.js \
 jquery.flot.selection.js jquery.flot.stack.js jquery.flot.symbol.js \
-jquery.flot.text.js > DRE.js
+jquery.flot.text.js > flot.DRE.js
